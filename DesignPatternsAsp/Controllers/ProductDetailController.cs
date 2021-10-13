3b50861e49439373cec13a9d4704b11e0f5fdfb5
DESKTOP-TCC4UPQ\user <santiago>
2021-10-12 19:30:13 -0500

    factory method

---
+++ DesignPatternsAsp/Controllers/ProductDetailController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tools.Earn;
+
+namespace DesignPatternsAsp.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.3m, 20);
+            //products
+            var localEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+            //total
+            ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+            return View();
+        }
+    }
+}
